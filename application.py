c5f5dd988fb430012bac03f3474561317d9afa15
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2020-03-28 15:35:46 +0530

    inserted values to the table

--- application.py
+++ application.py
@@ -5,6 +5,9 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import render_template
+from sqlalchemy.exc import SQLAlchemyError
+from models import *;
+import datetime
 
 app = Flask(__name__)
 
@@ -20,7 +23,7 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-
+session=db()
 @app.route("/")
 def index():
     return "Project 1: TODO"
@@ -29,7 +32,19 @@ def index():
 def register():
     # # form = request.form
     if request.method == "POST":
-        print("Name: ",request.form['username'])
-        print("Passowrd: ",request.form['password'])
-        return "<h2 text-align=\"center\">Hello "+request.form['username']+", Welcome !</h2>"
+        user_name=request.form['username']
+        password = request.form['password']
+        timestamp = datetime.datetime.now()
+        # print(timestamp)
+        try:
+            new_user = User(username= user_name, password = password, timestamp = timestamp) 
+            session.add(new_user)
+            session.commit()
+            return "<h2>Registered successfully, Please Login</h2>"
+
+        except SQLAlchemyError as e:
+            message = str(e.__dict__['orig'])
+            return "<h2>message</h2>"
     return render_template('registration.html')
+
+
